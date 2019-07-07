bc2a200c215e4e9bcd8e88bd4b4306df1800a282
Brian vanderbusch <brian.vanderbusch@gmail.com>
2019-07-07 10:54:05 -0500

    update probe geometry

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -908,8 +908,8 @@
  *   (0,0)
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -41  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -18  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER -14
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.5   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 20
